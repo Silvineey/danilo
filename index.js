169488ef951c0a3d901a5ddd87f532efe4410be8
Silvineey <lucreciasilvio@hotmail.com>
2025-12-27 22:25:56 -0300

    ajuste

---
+++ index.js
@@ -0,0 +1,20 @@
+const botoes = document.querySelectorAll(".slide p");
+const slides = document.querySelectorAll(".backgroundmain");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        const alvo = botao.dataset.slide;
+        const url = `assets/${alvo}.jpg`;
+
+        slides.forEach(slide => {
+            slide.classList.remove("on");
+            slide.classList.add("off");
+            if (slide.dataset.slide === alvo) {
+                slide.classList.remove("off");
+                slide.classList.add("on");
+                const capa = slide.querySelector(".capa");
+                capa.style.backgroundImage = `url("${url}")`;
+            }
+        });
+    });
+});
