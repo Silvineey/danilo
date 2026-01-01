e845bb14c528ba840153f0b959fd65a67177d2a3
Silvineey <lucreciasilvio@hotmail.com>
2026-01-01 10:36:49 -0300

    ajustes
    ajustes

---
+++ codigos/portfolio.js
@@ -0,0 +1,9 @@
+const caixas=document.querySelectorAll(".caixapreta")
+caixas.forEach(caixa=>{
+    caixa.addEventListener("click",(e)=>{
+        const alvo=e.currentTarget.dataset.galeria
+        if(alvo){
+            window.location.href=`/paginas/portfolios/${alvo}.html`
+        }
+    })
+})
